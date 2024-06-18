b161530f5bbbc146d48eb075038512faf9682146
Weebaay <jeanpaul.dijeont@gmail.com>
2024-06-18 14:50:20 +0200

    print alapha

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main imprime l'alphabet en minuscules suivi d'une nouvelle ligne
+ * 
+ * Return: Toujours 0
+ */
+int main(void)
+{
+    char minus = 'a';
+
+    while (minus <= 'z')
+    {
+        putchar(minus);
+        minus++;
+    }
+    putchar('\n');
+    return (0);
+}
