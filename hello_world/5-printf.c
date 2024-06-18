b81a6e502fe58f026dd635e7c238aecdedcad73d
Weebaay <jeanpaul.dijeont@gmail.com>
2024-06-18 10:07:12 +0200

    My c Printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(){
-    printf("with proper grammar , but the  outcone is a piece of art,\n");
+    printf("with proper grammar, but the  outcone is a piece of art,\n");
     return 0;
 }
