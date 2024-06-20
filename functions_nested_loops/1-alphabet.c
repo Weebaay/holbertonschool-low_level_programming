6d2293f4c5d0b7cdafda37fb77908a3676fa5955
Weebaay <jeanpaul.dijeont@gmail.com>
2024-06-20 15:11:20 +0200

    print alpha

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*print_alphabet - prints the alphabet in lowercase
+*you can only use _putchar
+*followed by a new line
+*Return : 0 always
+ */
+
+void print_alphabet(void)
+{
+char c = 'a';
+
+{
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+}
+putchar('\n');
+}
