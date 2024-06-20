074e0579deb8f709e210c5958748b636135fe8ec
Weebaay <jeanpaul.dijeont@gmail.com>
2024-06-20 15:45:10 +0200

    alphabetx10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - prints 10 times the alphabet in lowercase
+*followed by a new line
+ */
+
+ void print_alphabet_x10(void)
+ {
+    int i;
+
+    while (i <= 10)
+    {
+        char c = 'a'; 
+        while (c <= 'z')
+        {
+            _putchar(c);
+            c++;
+        }
+        _putchar('\n');
+        i++;
+    }
+ }
