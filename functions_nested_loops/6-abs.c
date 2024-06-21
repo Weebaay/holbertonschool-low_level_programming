647428f24a82a2e3eda9f4c3429951e3881cd77f
Weebaay <jeanpaul.dijeont@gmail.com>
2024-06-21 11:35:35 +0200

    couts value

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _abs - computes the absolute valeur of a integer.
+*@c: return
+*
+*description : blabla
+*Return: 0 always
+ */
+
+int _abs(int c)
+{
+	if (c < 0)
+	{
+		return (-c);
+	}
+	else
+	{
+		return (c);
+	}
+	return (0);
+}
