844217a7f5e371e373da7203d909c258c786377b
Weebaay <jeanpaul.dijeont@gmail.com>
2024-06-24 10:21:01 +0200

    uppercase

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _isupper - cheks for uppercase character.
+*@c: cat caractere
+*
+*Return: 1 if c is uppercase or 0
+*/
+
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	return (0);
+}
---
+++ more_functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifndef main_h
+#define main_h
+#include <unistd.h>
+#include <stdio.h>
+
+
+int _putchar(char c);
+
+int _isupper(int c);
+
+#endif
