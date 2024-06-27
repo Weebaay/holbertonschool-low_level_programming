d00ccc19bc08a6cc36cc29c686a7c34fe9726bab
Weebaay <jeanpaul.dijeont@gmail.com>
2024-06-27 11:45:22 +0200

    commiting the changes made to gh.com

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -6,8 +6,6 @@
 
 int _putchar(char c);
 
-void reset_to_98(int *n);
-
 void swap_int(int *a, int *b);
 
 int _strlen(char *s);
@@ -16,8 +14,14 @@ void _puts(char *str);
 
 void print_rev(char *s);
 
-void rev_string(char *s);
-
 void puts2(char *str);
+
+void puts_half(char *str);
+
+char *_strcpy(char *dest, char *src);
+
+void print_array(int *a, int n);
+
+char *_strcat(char *dest, char *src);
 
 #endif
