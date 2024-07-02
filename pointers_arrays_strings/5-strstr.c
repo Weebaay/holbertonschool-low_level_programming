13ea0fa94e64e263275410a2817537abc836499e
Weebaay <jeanpaul.dijeont@gmail.com>
2024-07-02 11:15:02 +0200

    locates substring

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* *_strstr - function that locates substring
+*@haystack: is the string
+*@needle: is a substring
+*
+*Return : string haystack
+*/
+char *_strstr(char *haystack, char *needle)
+{
+	if (*needle == '\0')
+	{
+		return (haystack);
+	}
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *h = haystack;
+		char *n = needle;
+
+		while (*n == *h && *n != '\0')
+		{
+			h++;
+			n++;
+		}
+		if (*n == '\0')
+		{
+			return (haystack);
+		}
+	}
+	return (0);
+}
