29e5326eea9a4b922f068ae25d25869d61962fe5
Weebaay <jeanpaul.dijeont@gmail.com>
2024-07-04 15:44:16 +0200

    mulptipli

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+*main - that multiplies two numbers
+*@argc: argument count
+*@argv: argument of value
+*
+*Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", num1 * num2);
+
+	return (0);
+}
