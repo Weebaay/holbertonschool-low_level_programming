dee068b1a916fa2dd3b155529b64f89ae4a14919
Weebaay <jeanpaul.dijeont@gmail.com>
2024-07-07 16:27:02 +0200

    adds positif numb

---
+++ argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+*main - that adds positive number
+*@argc: argument count
+*@argv: argument of value
+*
+*Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i, j;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		char *ag = argv[i];
+
+		for (j = 0; ag[j] != '\0'; j++)
+		{
+			if (!isdigit(ag[j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(ag);
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
