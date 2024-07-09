3386c3ee39a642d56bf56051df5c90e47a115ae2
Weebaay <jeanpaul.dijeont@gmail.com>
2024-07-09 09:33:00 +0200

    my first malloc

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* *create_array - creates array of chars and initializes it with specific char
+*@size: the size of memory
+*@c: specific char
+*
+*Return: char
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	char *tab = malloc(size);
+
+	if (tab == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		tab[i] = c;
+	}
+	return (tab);
+}
