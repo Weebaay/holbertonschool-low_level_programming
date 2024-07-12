f651d0c32e6091d0f1a601de91efff788fe42f7c
Weebaay <jeanpaul.dijeont@gmail.com>
2024-07-12 12:10:37 +0200

    my mainH

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <limits.h>
+#include <string.h>
 
 
 int _putchar(char c);
@@ -20,5 +21,13 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 
 void *malloc_checked(unsigned int b);
+
+char *_strncat(char *dest, char *src, int n);
+
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
+char *_strcat(char *dest, char *src);
+
+int _strlen(char *s);
 
 #endif
