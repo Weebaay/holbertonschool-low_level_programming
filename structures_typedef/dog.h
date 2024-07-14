b2fc791e2f93d9b123c7c6f66dc102a37f5e6b93
Weebaay <jeanpaul.dijeont@gmail.com>
2024-07-14 10:53:22 +0200

    ma structure

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,12 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
-#include "main.h"
 
 /**
-*@struct - dog containt information about dog
+* struct dog - Structure contenant les informations sur un chien
+* @name: Nom du chien
+* @age: Âge du chien
+* @owner: Nom du propriétaire du chien
 *
-*The dog structure contains information about a dog,
-including its name,its age and the name of its owner
+* Description: Cette structure contient les informations de base
+* sur un chien, y compris son nom, son âge et le nom de son propriétaire.
 */
 
 typedef struct dog
@@ -14,7 +16,7 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-}
-dogt_t;
+}dog;
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
