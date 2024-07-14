a079ee6ee511fb6efa337be8bf1dd75701ed5e95
Weebaay <jeanpaul.dijeont@gmail.com>
2024-07-14 11:25:28 +0200

    display structur

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,36 @@
+#include "dog.h"
+#include "main.h"
+
+/**
+*print_dog - display information about 'dog' structure
+*@d: pointer to a dog structure
+*
+*Return: Void
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	if (d->name != NULL)
+	{
+		printf("name: %s\n", d->name);
+	}
+	else
+	{
+		printf("name: (nil)\n");
+	}
+
+		printf("Age: %1f\n", d->age);
+
+	if (d->owner != NULL)
+	{
+		printf("Owner: %s\n", d->owner);
+	}
+	else
+	{
+		printf("Owner: (nil)\n");
+	}
+}
