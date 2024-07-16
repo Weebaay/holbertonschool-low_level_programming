20e0a2ca73547331b1e2f01365e05c74e32209cc
Weebaay <jeanpaul.dijeont@gmail.com>
2024-07-16 10:32:57 +0200

    array of parameter

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - that execute a function given as a parameter
+ * @array: pointer to integers
+ * @size: size
+ * @action: pointer to function
+ *
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array == NULL || action == NULL)
+	{
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
