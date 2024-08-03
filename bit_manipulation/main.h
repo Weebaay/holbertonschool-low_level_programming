1b7f51574a50ba3c78a058d61aacd4041dc684a9
Weebaay <jeanpaul.dijeont@gmail.com>
2024-08-03 08:50:51 +0200

    print the binary

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -3,6 +3,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <unistd.h>
 
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 #endif
